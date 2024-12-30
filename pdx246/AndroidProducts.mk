0825a388d8322836c5cd7b94d9a21a1f7c2751a5
Your Name <you@example.com>
2024-12-30 22:39:46 +0200

    init commit

---
+++ pdx246/AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_pdx246.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_pdx246-user \
+    twrp_pdx246-userdebug \
+    twrp_pdx246-eng
